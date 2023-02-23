b0921d5254d5ccc0ea12c642b6caabe71fba6612
shehab20089 <shehabmohsen1995@gmail.com>
2023-02-23 10:26:20 +0200

    fix small  config issue

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default {
   root: path.resolve(__dirname, "src"),
 
   build: {
-    outDir: "../dist",
-    emptyOutDir: true,
+    outDir: "dist",
+
     rollupOptions: {
       input: {
         main: new URL("./src/index.html", import.meta.url).pathname,
