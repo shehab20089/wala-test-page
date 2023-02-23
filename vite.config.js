7070d73265532a02a954645218bd90c574dbed7b
shehab20089 <shehabmohsen1995@gmail.com>
2023-02-23 08:59:37 +0200

    configure Vite to handle call-us page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 const path = require("path");
 
 export default {
+  base: "./",
   root: path.resolve(__dirname, "src"),
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: new URL("./src/index.html", import.meta.url).pathname,
+        callUs: new URL("./src/call-us.html", import.meta.url).pathname,
+      },
+    },
+  },
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
